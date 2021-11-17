182894f3c2cf97388148fd626e9c43591d37caef
4x4software <94319652+4x4software@users.noreply.github.com>
2021-11-17 05:55:01 +0100

    Feature/emptyprojects (#1)
    * Added empty AndroidApp and Common projects
    
    * Added empty common test project
    
    * Add vscode settings files
    
    Co-authored-by: Hendrik Jan Runhaar

---
+++ RekenTest.Common/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace RekenTest.Common
+{
+    public class Class1
+    {
+    }
+}
---
+++ RekenTest.Common/Implementers/Problem.cs
@@ -0,0 +1,11 @@
+﻿using RekenTest.Common.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RekenTest.Common.Implementers
+{
+    public class Problem: IProblem
+    {
+    }
+}
