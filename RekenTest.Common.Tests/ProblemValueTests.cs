d0a8759a021b6a79e9ff9a59bcc9b6fadad24812
Hendrik Jan Runhaar <hrunhaar@everyangle.org>
2021-11-17 05:35:55 +0100

    Added empty common test project

---
+++ RekenTest.Common.Tests/ProblemValueTests.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace RekenTest.Common.Tests
+{
+    public class ProblemValueTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
---
+++ RekenTest.Common/DTO/ProblemValue.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RekenTest.Common.DTO
+{
+    public class ProblemValue
+    {
+    }
+}
