797ba81b3f7d35c0f4d7c97670651abe4c5cd65c
Hendrik Jan Runhaar <hrunhaar@everyangle.org>
2022-06-28 21:58:23 +0200

    Add empty new project RekenTest.HostCmdApp

---
+++ RekenTest.HostCmdApp/GenerateAnswerService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace RekenTest.HostCmdApp
+{
+    internal class GenerateAnswerService : BackgroundService
+    {
+        protected override Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ RekenTest.HostCmdApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using RekenTest.Common.Implementers;
+using System.IO;
+using System.Reflection;
+using RekenTest.HostCmdApp;
+
+IHostBuilder builder = Host.CreateDefaultBuilder(args)
+    .UseContentRoot(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location))
+    .ConfigureServices((hostContext, services) =>
+    {
+        services
+            .AddSingleton<Problem>()
+            .AddHostedService<GenerateAnswerService>();
+            //.AddOptions<Settings>().Bind(hostContext.Configuration);
+    });
+
+await builder
+    .RunConsoleAsync();
