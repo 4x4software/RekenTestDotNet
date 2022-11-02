2ecc6661f0b2ad032dda04702b172b6975671b74
Hendrik Jan Runhaar <info@4x4software.nl>
2022-11-02 10:25:22 +0100

    New RekenTest.Database project

---
+++ RekenTest.Database/Interfaces/IProblemData.cs
@@ -0,0 +1,17 @@
+﻿using RekenTest.Common.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RekenTest.Database.Interfaces
+{
+    public interface IProblemData
+    {
+        public int Id { get; set; }
+
+        public void Assign(IProblem inputValue);
+    }
+}
